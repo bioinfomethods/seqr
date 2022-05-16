606ad0d45fecaf2c684f6eb467e8dceec04da0c5
Tommy Li <tommy.li@firefire.co>
2022-05-16 10:59:15 +0000

    feat: Also backup reference_data_db

--- mcri_deploy/scripts/create_backup.sh
+++ mcri_deploy/scripts/create_backup.sh
@@ -10,14 +10,17 @@
         cd /home/seqr/seqr
 
         /usr/local/bin/docker-compose exec -T postgres /usr/lib/postgresql/12/bin/pg_dump -U postgres seqrdb | gzip -c > "/home/seqr/backups/seqrdb-${datestamp}.dmp.gz"
+        /usr/local/bin/docker-compose exec -T postgres /usr/lib/postgresql/12/bin/pg_dump -U postgres reference_data_db | gzip -c > "/home/seqr/backups/reference_data_db-${datestamp}.dmp.gz"
 
         cd /home/seqr/backups
 
         # We expect the last good backup is our current backup but it may not be - so we only copy the most recent one that has
         # expected size to the cloud
-        LAST_GOOD_BACKUP=$(find /home/seqr/backups -size +15000 -iname '*.dmp.gz' | xargs ls -t -1 | head -n 1)
+        SEQRDB_LAST_GOOD_BACKUP=$(find /home/seqr/backups -size +15000 -iname 'seqrdb*.dmp.gz' | xargs ls -t -1 | head -n 1)
+        REFERENCEDB_LAST_GOOD_BACKUP=$(find /home/seqr/backups -size +15000 -iname 'reference_data_db*.dmp.gz' | xargs ls -t -1 | head -n 1)
 
-        gsutil cp "${LAST_GOOD_BACKUP}" gs://mcri-seqr-backups/
+        gsutil cp "${SEQRDB_LAST_GOOD_BACKUP}" gs://mcri-seqr-backups/
+        gsutil cp "${REFERENCEDB_LAST_GOOD_BACKUP}" gs://mcri-seqr-backups/
 
         echo "Done creating backup: $datestamp"
 
