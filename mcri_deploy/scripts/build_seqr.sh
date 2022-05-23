b54ca057035b8bd8c4b5c5f2e580b2a5f07e91ec
Tommy Li <tommy.li@firefire.co>
2022-05-23 14:58:02 +0000

    feat: Build script for seqr-build instance

---
+++ mcri_deploy/scripts/build_seqr.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#
+# Launch archie agent and run as daemon using jsvc
+
+set -eo pipefail
+
+#######################################
+# info() and error() log messages to STDOUT and STDERR respectively.
+# Globals:
+#   None
+# Arguments:
+#   Variable args written out as string
+# Outputs:
+#   Writes message to STDOUT or STDERR
+#######################################
+function info() {
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z') INFO $(basename "$0")] $*" >&1
+}
+
+function error() {
+    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z') ERROR $(basename "$0")] $*" >&2
+}
+
+set -euo pipefail
+
+check_command() {
+    if ! command -v "$1" &> /dev/null
+    then
+        error "$1 could not be found"
+        exit 3
+    fi
+}
+
+check_command "git"
+check_command "docker"
+check_command "docker-compose"
+
+build() {
+    PROJECT_DIR="$HOME/$1"
+    if [ ! -d "$PROJECT_DIR" ]; then
+      error "$PROJECT_DIR does not exist"
+      exit 3
+    fi
+    cd "$PROJECT_DIR"
+    info "Running build for $PROJECT_DIR"
+
+    git pull
+
+    COMPOSE_FILE="$PROJECT_DIR/mcri_deploy/docker-compose/docker-compose.yml"
+    COMPOSE_BUILD_FILE="$PROJECT_DIR/mcri_deploy/docker-compose/docker-compose.build.yml"
+
+    cp "$PROJECT_DIR/mcri_deploy/docker-compose/seqr.template.env" "$PROJECT_DIR/.env" 
+    COMPOSE_ENV_FILE="$PROJECT_DIR/.env"
+    
+    for line in $(grep -v '^#' "$PROJECT_DIR/.env" | xargs)
+    do
+        eval "$line"
+    done
+
+    # Build image and adds latest Docker tag by default
+    docker-compose --verbose \
+      -f "$COMPOSE_FILE" \
+      -f "$COMPOSE_BUILD_FILE" \
+      --env-file="$COMPOSE_ENV_FILE" \
+      build
+
+    # Tagging
+    SEQR_GIT_BRANCH_NAME=$(git branch --format='%(refname:short)')
+    SEQR_GIT_BRANCH_TAG=${SEQR_GIT_BRANCH_NAME/\//_}
+    SEQR_LONG_GIT_TAG=$(git describe --long)
+    docker-compose --verbose \
+      -f "$COMPOSE_FILE" \
+      -f "$COMPOSE_BUILD_FILE" \
+      --env-file="$COMPOSE_ENV_FILE" \
+      push
+
+    docker tag "$CONTAINER_REGISTRY/$SEQR_IMAGE_NAME:$SEQR_IMAGE_TAG" "$CONTAINER_REGISTRY/$SEQR_IMAGE_NAME:$SEQR_GIT_BRANCH_TAG"
+    docker tag "$CONTAINER_REGISTRY/$POSTGRES_IMAGE_NAME:$POSTGRES_IMAGE_TAG" "$CONTAINER_REGISTRY/$POSTGRES_IMAGE_NAME:$SEQR_GIT_BRANCH_TAG"
+    docker tag "$CONTAINER_REGISTRY/$REDIS_IMAGE_NAME:$REDIS_IMAGE_TAG" "$CONTAINER_REGISTRY/$REDIS_IMAGE_NAME:$SEQR_GIT_BRANCH_TAG"
+    docker tag "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$PIPELINE_RUNNER_IMAGE_TAG" "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SEQR_GIT_BRANCH_TAG"
+    docker push "$CONTAINER_REGISTRY/$SEQR_IMAGE_NAME:$SEQR_GIT_BRANCH_TAG"
+    docker push "$CONTAINER_REGISTRY/$POSTGRES_IMAGE_NAME:$SEQR_GIT_BRANCH_TAG"
+    docker push "$CONTAINER_REGISTRY/$REDIS_IMAGE_NAME:$SEQR_GIT_BRANCH_TAG"
+    docker push "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SEQR_GIT_BRANCH_TAG"
+
+    docker tag "$CONTAINER_REGISTRY/$SEQR_IMAGE_NAME:$SEQR_IMAGE_TAG" "$CONTAINER_REGISTRY/$SEQR_IMAGE_NAME:$SEQR_LONG_GIT_TAG"
+    docker tag "$CONTAINER_REGISTRY/$POSTGRES_IMAGE_NAME:$POSTGRES_IMAGE_TAG" "$CONTAINER_REGISTRY/$POSTGRES_IMAGE_NAME:$SEQR_LONG_GIT_TAG"
+    docker tag "$CONTAINER_REGISTRY/$REDIS_IMAGE_NAME:$REDIS_IMAGE_TAG" "$CONTAINER_REGISTRY/$REDIS_IMAGE_NAME:$SEQR_LONG_GIT_TAG"
+    docker tag "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$PIPELINE_RUNNER_IMAGE_TAG" "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SEQR_LONG_GIT_TAG"
+    docker push "$CONTAINER_REGISTRY/$SEQR_IMAGE_NAME:$SEQR_LONG_GIT_TAG"
+    docker push "$CONTAINER_REGISTRY/$POSTGRES_IMAGE_NAME:$SEQR_LONG_GIT_TAG"
+    docker push "$CONTAINER_REGISTRY/$REDIS_IMAGE_NAME:$SEQR_LONG_GIT_TAG"
+    docker push "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SEQR_LONG_GIT_TAG"
+
+    if [[ "$1" == "seqr" ]]; then
+        SEQR_VERSION="v$(date +"%Y.%m.%d")_00"
+        docker tag "$CONTAINER_REGISTRY/$SEQR_IMAGE_NAME:$SEQR_IMAGE_TAG" "$CONTAINER_REGISTRY/$SEQR_IMAGE_NAME:$SEQR_VERSION"
+        docker tag "$CONTAINER_REGISTRY/$POSTGRES_IMAGE_NAME:$POSTGRES_IMAGE_TAG" "$CONTAINER_REGISTRY/$POSTGRES_IMAGE_NAME:$SEQR_VERSION"
+        docker tag "$CONTAINER_REGISTRY/$REDIS_IMAGE_NAME:$REDIS_IMAGE_TAG" "$CONTAINER_REGISTRY/$REDIS_IMAGE_NAME:$SEQR_VERSION"
+        docker tag "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$PIPELINE_RUNNER_IMAGE_TAG" "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SEQR_VERSION"
+        docker push "$CONTAINER_REGISTRY/$SEQR_IMAGE_NAME:$SEQR_VERSION"
+        docker push "$CONTAINER_REGISTRY/$POSTGRES_IMAGE_NAME:$SEQR_VERSION"
+        docker push "$CONTAINER_REGISTRY/$REDIS_IMAGE_NAME:$SEQR_VERSION"
+        docker push "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SEQR_VERSION"
+    fi
+}
+
+case "$1" in
+test)
+    build "seqr-test"
+    ;;
+prod)
+    build "seqr"
+    ;;
+*)
+    info "usage: build_seqr.sh {test|prod|info}"
+    exit 3
+    ;;
+esac
