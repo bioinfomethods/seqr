9268727cda3d4c779a15ce2a382312f9fea31925
tommy.li <tommy.li@firefire.co>
2023-05-24 13:52:15 +1000

    fix: Add more robust check for optional SOCIAL_AUTH_GROUP_EXCLUDE_PATTERNS settings

--- mcri_ext/security/social_auth_pipeline.py
+++ mcri_ext/security/social_auth_pipeline.py
@@ -1,6 +1,7 @@
 from seqr.utils.logging_utils import SeqrLogger
 
 logger = SeqrLogger(__name__)
+log = logger
 
 
 def associate_groups(backend, response, user, details, *args, **kwargs):
@@ -14,4 +15,43 @@ def associate_groups(backend, response, user, details, *args, **kwargs):
         #     db_group, _ = Group.objects.get_or_create(name=idp_group)
         #     user.groups.add(db_group)
     else:
-        logger.warning('Skipping associating groups as user was not given.')
+        log.warning('Skipping associating groups as user was not given.')
+
+
+def _group_matches_settings_exclude_patterns(group_name: str) -> bool:
+    if hasattr(settings,
+               'SOCIAL_AUTH_GROUP_EXCLUDE_PATTERNS') and settings.SOCIAL_AUTH_GROUP_EXCLUDE_PATTERNS and isinstance(
+            settings.SOCIAL_AUTH_GROUP_EXCLUDE_PATTERNS, list):
+        for pattern in settings.SOCIAL_AUTH_GROUP_EXCLUDE_PATTERNS:
+            if re.match(pattern, group_name):
+                return True
+
+    return False
+
+
+def associate_by_email_or_username(backend, details, user=None, *args, **kwargs):
+    """
+    This is only safe because we're using MCRI Okta where accounts are maintained by MCRI IT
+    and users cannot simply register themselves.  This means we can safely trust the email and username
+    association coming from MCRI Okta.
+    """
+    if user:
+        return None
+
+    email = details.get('email')
+    if email:
+        users = User.objects.filter(Q(username__iexact=email) | Q(email__iexact=email))
+
+        if len(users) == 0:
+            return None
+        elif len(users) > 1:
+            raise AuthException(backend, 'The given email address is associated with another account')
+        else:
+            return {'user': users[0],
+                    'is_new': False}
+
+
+def log_authentication(backend, response, is_new=False, *args, **kwargs):
+    log.info('Logged in {} ({})'.format(response['email'], backend.name))
+    if is_new:
+        log.info('Created user {} ({})'.format(response['email'], backend.name))
