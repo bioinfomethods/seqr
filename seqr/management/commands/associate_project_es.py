a1fe1986cfc2c434cdbffe2d6cf7a8f7971825d7
tommy.li <tommy.li@firefire.co>
2023-07-06 13:56:42 +1000

    fix: Fixed associate_project_es.py after Seqr merge

--- seqr/management/commands/associate_project_es.py
+++ seqr/management/commands/associate_project_es.py
@@ -1,14 +1,12 @@
 import logging
 import sys
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db.models import Q
-from django.utils import timezone
 
 from seqr.models import Project
-from seqr.views.utils import variant_utils
-from seqr.views.utils.dataset_utils import match_and_update_samples, \
-    validate_index_metadata_and_get_elasticsearch_index_samples, load_mapping_file
+from seqr.utils.search.add_data_utils import add_new_search_samples
 
 logger = logging.getLogger(__name__)
 
@@ -29,41 +27,17 @@ class Command(BaseCommand):
         mapping_file_path = options.get('mapping')
         project = Project.objects.filter(Q(guid__iexact=given_project) | Q(name__iexact=given_project)).first()
 
+        payload = {
+            'datasetType': DATASET_TYPE,
+            'elasticsearchIndex': index,
+            'mappingFilePath': mapping_file_path,
+        }
+        url = f'{settings.BASE_URL}project/{project.guid}/project_page'
+        summary_template = '{num_new_samples} new {sample_type}{dataset_type} samples are loaded in ' + url
         try:
-            sample_ids, sample_type = validate_index_metadata_and_get_elasticsearch_index_samples(
-                index, project=project, dataset_type=DATASET_TYPE)
-            logger.info('Validated index=%s, project=%s, sample_ids=%s, sample_type=%s', index, project.guid,
-                        sample_ids, sample_type)
-            if not sample_ids:
-                raise ValueError('No samples found in the index. Make sure the specified caller type is correct')
-
-            sample_id_to_individual_id_mapping = load_mapping_file(mapping_file_path,
-                                                                   None) if mapping_file_path else {}
+            num_sample, inactivated_sample_guids, updated_family_guids, updated_samples, summary_message = add_new_search_samples(
+                payload, project, None, summary_template=summary_template)
+            logger.info(summary_message)
         except ValueError as e:
             logger.error(str(e), exc_info=e)
             sys.exit(1)
-
-        data_source = 'archie'
-        ignore_extra_samples = False
-        try:
-            samples, matched_individual_ids, activated_sample_guids, inactivated_sample_guids, updated_family_guids, remaining_sample_ids = match_and_update_samples(
-                projects=[project],
-                user=None,
-                sample_ids=sample_ids,
-                elasticsearch_index=index,
-                sample_type=sample_type,
-                data_source=data_source,
-                dataset_type=DATASET_TYPE,
-                sample_id_to_individual_id_mapping=sample_id_to_individual_id_mapping,
-                raise_no_match_error=ignore_extra_samples,
-                raise_unmatched_error_template=None if ignore_extra_samples else 'Matches not found for ES sample ids: {sample_ids}. Uploading a mapping file for these samples, or select the "Ignore extra samples in callset" checkbox to ignore.'
-            )
-        except ValueError as e:
-            logger.error(str(e), exc_info=e)
-            sys.exit(1)
-
-        variant_utils.reset_cached_search_results(project)
-        logger.info(
-            'Successfully associated ES index=%s with project=%s.  samples=%s, matched_individual_ids=%s, activated_sample_guids=%s, inactivated_sample_guids=%s, updated_family_guids=%s, remaining_sample_ids=%s',
-            index, project, [s.sample_id for s in samples], matched_individual_ids, activated_sample_guids,
-            inactivated_sample_guids, updated_family_guids, remaining_sample_ids)
