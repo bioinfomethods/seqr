9b100ac399ce1a52c8168a0a9355374c03d6bab2
tommy.li <tommy.li@firefire.co>
2023-07-14 08:24:30 +1000

    fix: Fixed Seqr create_backup.sh script
    * Use docker compose v2 CLI syntax
    * Use Postgres 13 libs

--- mcri_deploy/scripts/create_backup.sh
+++ mcri_deploy/scripts/create_backup.sh
@@ -12,8 +12,8 @@ echo "Creating backups to $BACKUP_PATH with date $datestamp"
 SEQRDB_BACKUP=$BACKUP_PATH/seqrdb-$datestamp.dmp.gz
 REFERENCE_DATA_DB_BACKUP=$BACKUP_PATH/reference_data_db-$datestamp.dmp.gz
 
-/usr/local/bin/docker-compose -f /home/seqr/seqr/docker-compose.yml --env-file /home/seqr/seqr/.env exec -T postgres /usr/lib/postgresql/12/bin/pg_dump -U postgres seqrdb | gzip -c > "$SEQRDB_BACKUP"
-/usr/local/bin/docker-compose -f /home/seqr/seqr/docker-compose.yml --env-file /home/seqr/seqr/.env exec -T postgres /usr/lib/postgresql/12/bin/pg_dump -U postgres reference_data_db | gzip -c > "$REFERENCE_DATA_DB_BACKUP"
+/usr/bin/docker compose -f /home/seqr/seqr/docker-compose.yml --env-file /home/seqr/seqr/.env exec -T postgres /usr/lib/postgresql/13/bin/pg_dump -U postgres seqrdb | gzip -c > "$SEQRDB_BACKUP"
+/usr/bin/docker compose -f /home/seqr/seqr/docker-compose.yml --env-file /home/seqr/seqr/.env exec -T postgres /usr/lib/postgresql/13/bin/pg_dump -U postgres reference_data_db | gzip -c > "$REFERENCE_DATA_DB_BACKUP"
 
 gsutil cp "${SEQRDB_BACKUP}" gs://mcri-seqr-backups/
 echo "Backed up $SEQRDB_BACKUP to gs://mcri-seqr-backups/"
