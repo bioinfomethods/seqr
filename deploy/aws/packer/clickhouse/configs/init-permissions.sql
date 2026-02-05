fe64ac117736cee12faade227742c8ba1c32ba40
Simon Sadedin <simon.sadedin@mcri.edu.au>
2026-02-05 22:19:43 +1100

    add actual clickhouse configs

--- deploy/aws/packer/clickhouse/configs/init-permissions.sql
+++ deploy/aws/packer/clickhouse/configs/init-permissions.sql
@@ -1,9 +1,2 @@
--- Placeholder init-permissions.sql - replace with your actual initialization SQL
--- This file runs on first container startup
-
--- Example: Create database
--- CREATE DATABASE IF NOT EXISTS seqr;
-
--- Example: Create user
--- CREATE USER IF NOT EXISTS seqr_user IDENTIFIED BY 'password';
--- GRANT ALL ON seqr.* TO seqr_user;
+GRANT NAMED COLLECTION ADMIN ON *.* TO seqr_user;
+GRANT SHOW NAMED COLLECTIONS ON *.* TO seqr_user;
